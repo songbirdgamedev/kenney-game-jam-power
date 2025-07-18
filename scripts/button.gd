54637d425888c03d7065c30184bbdf6b976b1b28
emery <215300895+xexemery@users.noreply.github.com>
2025-07-18 23:33:05 +0100

    Add light box linked to button

--- scripts/button.gd
+++ scripts/button.gd
@@ -3,13 +3,16 @@ extends StaticBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape_default: CollisionShape2D = $CollisionShapeDefault
-@onready var collision_shape_pressed: CollisionShape2D = $CollisionShapePressed
 @onready var button_area: Area2D = $Area2D
+
+
+signal button_pressed()
 
 
 func _on_button_press(body: Node2D) -> void:
 	if body is CharacterBody2D:
 		animated_sprite.play("press")
+		button_pressed.emit()
 		_disable_button()
 
 
