0b582b03209f629a25ada619543395d9170f1570
emery <215300895+xexemery@users.noreply.github.com>
2025-07-20 09:41:11 +0100

    Rework lightbox classes

--- scripts/lights/action_light_box.gd
+++ scripts/lights/action_light_box.gd
@@ -1,27 +1,2 @@
 class_name ActionLightBox
 extends LightBox
-
-
-@onready var big_box: CollisionShape2D = $Area2D/BigBox
-
-
-func _turn_on() -> void:
-	is_on = true
-	animated_sprite.play("turn_on")
-	big_box.set_deferred("disabled", false)
-
-	await get_tree().create_timer(0.5).timeout
-	turned_on.emit()
-
-	big_box.set_deferred("disabled", true)
-
-
-func _turn_off() -> void:
-	is_on = false
-	animated_sprite.play("turn_off")
-	big_box.set_deferred("disabled", false)
-
-	await get_tree().create_timer(0.5).timeout
-	turned_off.emit()
-
-	big_box.set_deferred("disabled", true)
--- scripts/lights/light_box.gd
+++ scripts/lights/light_box.gd
@@ -2,31 +2,24 @@ class_name LightBox
 extends StaticBody2D
 
 
+@onready var lights: Node2D = $".."
 @onready var light_box: LightBox = $"."
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var box_area: Area2D = $Area2D
+@onready var next_level_door: StaticBody2D = $"../../NextLevelDoor"
 
 @export var is_on: bool = false
-@export var changed: bool = false
-
-
-signal turned_on()
-signal turned_off()
 
 
 func _on_button_press() -> void:
 	await get_tree().create_timer(0.1).timeout
-	turn_on_off(true)
+	turn_on_off()
+	print("finished")
+	await get_tree().create_timer(0.5).timeout
+	_are_all_lights_on()
 
 
-func turn_on_off(first: bool) -> void:
-	# handle first case differently to subsequent cases
-	# subsequent cases triggering a function light on can also turn lights that have been changed already
-	if changed:
-		return
-
-	changed = true
-
+func turn_on_off() -> void:
 	if is_on:
 		_turn_off()
 	else:
@@ -36,10 +29,21 @@ func turn_on_off(first: bool) -> void:
 func _turn_on() -> void:
 	is_on = true
 	animated_sprite.play("turn_on")
-	turned_on.emit()
+	await get_tree().create_timer(0.5).timeout
+
+	for light in box_area.get_overlapping_bodies():
+		if light != self and light is LightBox:
+			light.turn_on_off()
 
 
 func _turn_off() -> void:
 	is_on = false
 	animated_sprite.play("turn_off")
-	turned_off.emit()
+	await get_tree().create_timer(0.5).timeout
+
+
+func _are_all_lights_on() -> void:
+	print("checking")
+	if lights.get_children().all(func(light): return light.is_on):
+		await get_tree().create_timer(0.5).timeout
+		next_level_door.open_door()
